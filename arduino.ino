80c1f0c83412efe0e92c3ee837dc60664868435e
LucasNagasse <lucas.nagasse@sptech.school>
2024-04-09 11:52:42 -0300

    Código do arduino

---
+++ arduino.ino
@@ -0,0 +1,15 @@
+//Sensor de temperatura usando o LM35
+const int pino = A0; // Define o pino que lera a saída do LM35
+float temperatura; // Variável que armazenará a temperatura medida
+
+//Função que será executada uma vez quando ligar ou resetar o Arduino
+void setup() {
+  Serial.begin(9600); // inicializa a comunicação serial
+}
+//Função que será executada continuamente
+void loop() {
+  temperatura = (float analogRead(pino))*0.49; // 0.49V ~= 1ºC
+  Serial.print("Temperatura: ");
+  Serial.println(temperatura);
+  delay(500);
+}

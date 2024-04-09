dd1b0b30921cd4702427788476581eef33743bf3
LucasNagasse <lucas.nagasse@sptech.school>
2024-04-09 11:59:23 -0300

    adição da API da Marise

---
+++ dat-acqu-ino/main.js
@@ -0,0 +1,139 @@
+// declaração das bibliotecas:
+const serialport = require('serialport'); // biblioteca de comunicação serial (com o arduino)
+const express = require('express'); // biblioteca de criação de servidor local
+const mysql = require('mysql2'); // biblioteca de integração com o banco de dados MySQL
+
+const SERIAL_BAUD_RATE = 9600; // define a taxa de transmissão de dados em Bauds
+const SERVIDOR_PORTA = 3300; // define a porta de funcionamento do servidor
+
+// configure a linha abaixo caso queira que os dados capturados sejam inseridos no banco de dados.
+// false -> nao insere
+// true -> insere
+const HABILITAR_OPERACAO_INSERIR = false;
+
+const serial = async (
+    valoresDht11Umidade,
+    valoresDht11Temperatura,
+    valoresLuminosidade,
+    valoresLm35Temperatura,
+    valoresChave
+) => {
+    let poolBancoDados = ''
+
+    poolBancoDados = mysql.createPool(
+        {
+            // altere!
+            // CREDENCIAIS DO BANCO - MYSQL WORKBENCH
+            host: 'localhost',
+            user: 'USUARIO_DO_BANCO_LOCAL',
+            password: 'SENHA_DO_BANCO_LOCAL',
+            database: 'DATABASE_LOCAL'
+        }
+    ).promise();
+
+    const portas = await serialport.SerialPort.list();
+    const portaArduino = portas.find((porta) => porta.vendorId == 2341 && porta.productId == 43);
+    if (!portaArduino) {
+        throw new Error('O arduino não foi encontrado em nenhuma porta serial');
+    }
+    const arduino = new serialport.SerialPort(
+        {
+            path: portaArduino.path,
+            baudRate: SERIAL_BAUD_RATE
+        }
+    );
+    arduino.on('open', () => {
+        console.log(`A leitura do arduino foi iniciada na porta ${portaArduino.path} utilizando Baud Rate de ${SERIAL_BAUD_RATE}`);
+    });
+    arduino.pipe(new serialport.ReadlineParser({ delimiter: '\r\n' })).on('data', async (data) => {
+        //console.log(data);
+        const valores = data.split(';');
+        const dht11Umidade = parseFloat(valores[0]);
+        const dht11Temperatura = parseFloat(valores[1]);
+        const lm35Temperatura = parseFloat(valores[2]);
+        const luminosidade = parseFloat(valores[3]);
+        const chave = parseInt(valores[4]);
+
+        valoresDht11Umidade.push(dht11Umidade);
+        valoresDht11Temperatura.push(dht11Temperatura);
+        valoresLuminosidade.push(luminosidade);
+        valoresLm35Temperatura.push(lm35Temperatura);
+        valoresChave.push(chave);
+
+        if (HABILITAR_OPERACAO_INSERIR) {
+
+            // altere!
+            // Este insert irá inserir os dados na tabela "medida"
+            // -> altere nome da tabela e colunas se necessário
+            // Este insert irá inserir dados de fk_aquario id=1 (fixo no comando do insert abaixo)
+            // >> você deve ter o aquario de id 1 cadastrado.
+            await poolBancoDados.execute(
+                'INSERT INTO medida (dht11_umidade, dht11_temperatura, luminosidade, lm35_temperatura, chave) VALUES (?, ?, ?, ?, ?)',
+                [dht11Umidade, dht11Temperatura, luminosidade, lm35Temperatura, chave]
+            );
+            console.log("valores inseridos no banco: ", dht11Umidade + ", " + dht11Temperatura + ", " + luminosidade + ", " + lm35Temperatura + ", " + chave)
+        
+        }
+        
+    });
+    arduino.on('error', (mensagem) => {
+        console.error(`Erro no arduino (Mensagem: ${mensagem}`)
+    });
+}
+
+
+// não altere!
+const servidor = (
+    valoresDht11Umidade,
+    valoresDht11Temperatura,
+    valoresLuminosidade,
+    valoresLm35Temperatura,
+    valoresChave
+) => {
+    const app = express();
+    app.use((request, response, next) => {
+        response.header('Access-Control-Allow-Origin', '*');
+        response.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+        next();
+    });
+    app.listen(SERVIDOR_PORTA, () => {
+        console.log(`API executada com sucesso na porta ${SERVIDOR_PORTA}`);
+    });
+    app.get('/sensores/dht11/umidade', (_, response) => {
+        return response.json(valoresDht11Umidade);
+    });
+    app.get('/sensores/dht11/temperatura', (_, response) => {
+        return response.json(valoresDht11Temperatura);
+    });
+    app.get('/sensores/luminosidade', (_, response) => {
+        return response.json(valoresLuminosidade);
+    });
+    app.get('/sensores/lm35/temperatura', (_, response) => {
+        return response.json(valoresLm35Temperatura);
+    });
+    app.get('/sensores/chave', (_, response) => {
+        return response.json(valoresChave);
+    });
+}
+
+(async () => {
+    const valoresDht11Umidade = [];
+    const valoresDht11Temperatura = [];
+    const valoresLuminosidade = [];
+    const valoresLm35Temperatura = [];
+    const valoresChave = [];
+    await serial(
+        valoresDht11Umidade,
+        valoresDht11Temperatura,
+        valoresLuminosidade,
+        valoresLm35Temperatura,
+        valoresChave
+    );
+    servidor(
+        valoresDht11Umidade,
+        valoresDht11Temperatura,
+        valoresLuminosidade,
+        valoresLm35Temperatura,
+        valoresChave
+    );
+})();

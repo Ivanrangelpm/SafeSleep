53aff003f72e259e31ac7f7ceba6c02a3f16708d
Ivanrangelpm <ivan.marcolin@sptech.school>
2024-04-16 15:05:02 -0300

    Trocando api

---
+++ dat-acqu-ino/main.js
@@ -0,0 +1,123 @@
+// Importa os módulos necessários
+// não altere!
+const serialport = require('serialport'); // Módulo para comunicação serial
+const express = require('express'); // Módulo para criar um servidor web
+const mysql = require('mysql2'); // Módulo para conectar ao MySQL
+
+// Constantes para configurações
+// não altere!
+const SERIAL_BAUD_RATE = 9600;
+const SERVIDOR_PORTA = 3300;
+
+// Habilita ou desabilita a inserção de dados no banco de dados
+// false -> nao insere
+// true -> insere
+const HABILITAR_OPERACAO_INSERIR = true;
+
+// Função para comunicação serial
+const serial = async (
+    valoresLm35Temperatura
+) => {
+    let poolBancoDados = ''
+
+    // Conexão com o banco de dados MySQL
+    poolBancoDados = mysql.createPool(
+        {
+            // altere!
+            // Credenciais do banco de dados
+            host: '10.18.36.3',
+            user: 'aluno',
+            password: 'Sptech#2024',
+            database: 'exemplo',
+            port: 3307
+        }
+    ).promise();
+
+    // Lista as portas seriais disponíveis e procura pelo Arduino
+    const portas = await serialport.SerialPort.list();
+    const portaArduino = portas.find((porta) => porta.vendorId == 2341 && porta.productId == 43);
+    if (!portaArduino) {
+        throw new Error('O arduino não foi encontrado em nenhuma porta serial');
+    }
+
+    // Configura a porta serial com o baud rate especificado
+    const arduino = new serialport.SerialPort(
+        {
+            path: portaArduino.path,
+            baudRate: SERIAL_BAUD_RATE
+        }
+    );
+
+    // Evento quando a porta serial é aberta
+    arduino.on('open', () => {
+        console.log(`A leitura do arduino foi iniciada na porta ${portaArduino.path} utilizando Baud Rate de ${SERIAL_BAUD_RATE}`);
+    });
+
+    // Processa os dados recebidos do Arduino
+    arduino.pipe(new serialport.ReadlineParser({ delimiter: '\r\n' })).on('data', async (data) => {
+        console.log(data);
+        const valores = data.split(';');
+        const lm35Temperatura = parseFloat(valores[0]);
+        valoresLm35Temperatura.push(lm35Temperatura);
+
+        // Insere os dados no banco de dados (se habilitado)
+        if (HABILITAR_OPERACAO_INSERIR) {
+
+            // altere!
+            // Este insert irá inserir os dados na tabela "medida"
+            await poolBancoDados.execute(
+                'INSERT INTO medida (temperatura) VALUES (?)',
+                [lm35Temperatura]
+            );
+            console.log("valores inseridos no banco: ", lm35Temperatura)
+        
+        }
+        
+    });
+
+    // Evento para lidar com erros na comunicação serial
+    arduino.on('error', (mensagem) => {
+        console.error(`Erro no arduino (Mensagem: ${mensagem}`)
+    });
+}
+
+
+// não altere!
+// Função para criar e configurar o servidor web
+const servidor = (
+    valoresLm35Temperatura
+) => {
+    const app = express();
+
+    // Configurações de CORS
+    app.use((request, response, next) => {
+        response.header('Access-Control-Allow-Origin', '*');
+        response.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+        next();
+    });
+
+    // Inicia o servidor na porta especificada
+    app.listen(SERVIDOR_PORTA, () => {
+        console.log(`API executada com sucesso na porta ${SERVIDOR_PORTA}`);
+    });
+
+    app.get('/sensores/lm35/temperatura', (_, response) => {
+        return response.json(valoresLm35Temperatura);
+    });
+}
+
+// Função principal assíncrona para iniciar a comunicação serial e o servidor web
+(async () => {
+    // Arrays para armazenar os valores dos sensores
+    const valoresLm35Temperatura = [];
+
+    // Inicia a comunicação serial
+    await serial(
+        valoresLm35Temperatura
+    );
+
+    // Inicia o servidor web
+    servidor(
+        valoresLm35Temperatura
+    );
+})();

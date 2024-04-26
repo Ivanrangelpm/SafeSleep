8638790abb7987875d94a548851c93560c24a2ac
Bruno Yuji <160349419+bruno-yuji@users.noreply.github.com>
2024-04-26 13:54:30 -0300

    atualizando script BD

--- BancoDeDados/createDatabase_v3.sql
+++ BancoDeDados/createDatabase_v3.sql
@@ -1,12 +1,19 @@
-create database SafeSleep;
-use SafeSleep;
+CREATE DATABASE SafeSleep;
+USE SafeSleep;
 
 CREATE TABLE sensor (
-idSensor INT PRIMARY KEY AUTO_INCREMENT
+idSensor INT PRIMARY KEY
 );
 
-create table hospital(
-  idHospital int primary key not null auto_increment,
+INSERT INTO sensor VALUES
+	(1),
+	(2),
+	(3),
+	(4),
+	(5);
+
+CREATE TABLE hospital(
+idHospital int primary key not null auto_increment,
   razaoSocial varchar(100) not null,
   nomeFantasia varchar(100),
   cnpj char(14) not null,
@@ -16,7 +23,12 @@ create table hospital(
   numero varchar(45)not null
 );
 
-create table usuario(
+INSERT INTO hospital VALUES 
+(default, "Familia Feliz", "HospitalTech", "23456789101213", "hospitalTech@gmail.com", "11999999999", "11111000", "500"),
+(default, "Familia Contente", "Sptechospital", "75920391645378", "SpTechospital@gmail.com", "11777777777", "11111222", "91"),
+(default, "Familia Happy", "SaoPedro", "78946372849307", "SaoPedro@gmail.com", "11888888888", "11111444", "1000");
+
+CREATE TABLE usuario(
   idUsuario int not null auto_increment,
   login varchar(45) not null,
   senha varchar(45) not null,
@@ -26,11 +38,19 @@ create table usuario(
   sobrenome varchar(45),
   licenca varchar(45),
   fkHospital int not null,
+  constraint chkLicenca check (licenca like 'CRM/%'),
   constraint pkUsuarioHospital primary key (idUsuario, fkHospital),
   constraint fkUsuarioHospital foreign key (fkHospital) references hospital(idHospital)
 );
 
-create table incubadora(
+INSERT INTO usuario VALUES
+	(default, 'brandao@gmail.com', '1234567!', '85768548503', 'Diretor', 'Fernando', 'Brandão', 'CRM/SP 123456', 3),
+	(default, 'david@outlook.com', '2345678@', '38569845804', 'Diretor', 'David', 'Oliveira', 'CRM/SP 987654', 1),
+	(default, 'raul@yahoo.com.br', '8745643#', '75684295505', 'Médico', 'Raul', 'Gagliardo', 'CRM/SP 758692', 1),
+	(default, 'rebecca@gmail.com.br', '6789$647', '75849368502', 'Diretora', 'Rebecca', 'Andrade', 'CRM/SP 746583', 2);
+
+
+CREATE TABLE incubadora(
   codigoDeSerie int primary key not null,
   modelo varchar(50),
   fkHospital int not null,
@@ -39,7 +59,14 @@ create table incubadora(
   constraint fkIncubadoraHospital foreign key (fkHospital) references hospital(idHospital)
 );
 
-create table historico(
+INSERT INTO incubadora VALUES
+	(289, 1186, 1, 3),
+    (362, 1186, 2, 1),
+    (121, 1186, 3, 2),
+    (523, 1186, 2, 2),
+    (321, 1186, 1, 3);
+
+CREATE TABLE historico(
   idHistorico int not null auto_increment,
   dataHora date not null,
   temperatura decimal(5, 2) not null,
@@ -59,3 +86,10 @@ CREATE TABLE bebe(
   CONSTRAINT fkBebeIncubadora FOREIGN KEY(fkIncubadora) 
 	REFERENCES incubadora(idIncubadora)
 );
+
+INSERT INTO bebe VALUES 
+(default, 1, "Jorge", "Silva", "2024-05-01", "Masculino", 0),
+(default, 2, "Hellen", "Monteiro", "2024-04-26", "Feminino", 1),
+(default, 3, "Lua", "Santos", "2024-05-07", "Feminino", 0),
+(default, 4, "Melissa", "Bittencourt", "2024-05-03", "Feminino", 0),
+(default, 5, "Miguel", "Camargo", "2024-05-08", "Masculino", 1);
